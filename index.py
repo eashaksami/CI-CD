4c9d51429990243857b6b9e5423950175bf89636
eashaksami <46522742+eashaksami@users.noreply.github.com>
2021-12-07 20:01:43 +0600

    Update index.py

--- index.py
+++ index.py
@@ -5,17 +5,16 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return {
-        'msg': 'Hello World!',
-        'app_version': '4'
+        'msg': 'Hello Python',
+        'app_version': '1'
     }
 
 @app.route('/add/<a>/<b>')
 def add_route(a, b):
     return {
-        'sum': add(a, b)
+        'result': add(a, b)
     }
 
-# Input can be both strings and digits
 def add(a,b):
     if type(a) == str and not a.isdigit():
         return "Invalid Input"
@@ -28,13 +27,59 @@ def add(a,b):
     return a + b
 
 
+@app.route('/subtract/<a>/<b>')
+def remove_route(a, b):
+    return {
+        'result': subtract(a, b)
+    }
+
+# Input can be both strings and digits
+def subtract(a,b):
+    if type(a) == str and not a.isdigit():
+        return "Invalid Input"
+
+    if type(b) == str and not b.isdigit():
+        return "Invalid Input"
+
+    a = int(a)
+    b = int(b)
+    return a - b
+
+@app.route('/multiply/<a>/<b>')
+def remove_route(a, b):
+    return {
+        'result': multiply(a, b)
+    }
+
+# Input can be both strings and digits
+def multiply(a,b):
+    if type(a) == str and not a.isdigit():
+        return "Invalid Input"
+
+    if type(b) == str and not b.isdigit():
+        return "Invalid Input"
+
+    a = int(a)
+    b = int(b)
+    return a * b
+
+@app.route('/divide/<a>/<b>')
+def remove_route(a, b):
+    return {
+        'result': divide(a, b)
+    }
+
+# Input can be both strings and digits
+def divide(a,b):
+    if type(a) == str and not a.isdigit():
+        return "Invalid Input"
+
+    if type(b) == str and not b.isdigit():
+        return "Invalid Input"
+
+    a = int(a)
+    b = int(b)
+    return a / b
+
 if __name__ == "__main__":
     app.run()
-
-
-
-
-
-
-
-
