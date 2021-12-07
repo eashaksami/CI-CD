bef569be8c9b5b3b352bdbed58eb97235612e8fc
eashaksami <46522742+eashaksami@users.noreply.github.com>
2021-12-07 20:24:07 +0600

    Update test_index.py

--- test_index.py
+++ test_index.py
@@ -1,7 +1,23 @@
-from index import add
+import unittest
 
-def test_add():
-    assert add(1,2) == 3
-    assert add(1, "asdf") == "Invalid Input"
+from index import *
+import pytest
 
-    # assert add(1,"abc") == "Invalid Input"
+class test_index(unittest.TestCase):
+
+    def test_add(self):
+        assert add(1, 2) == 3
+        assert add(1, "asdf") == "Invalid Input"
+
+    def test_subtract(self):
+        assert subtract(1, 2) == 3
+        assert subtract(1, "asdf") == "Invalid Input"
+
+    def test_multiply(self):
+        assert multiply(1, 2) == 3
+        assert multiply(1, "asdf") == "Invalid Input"
+
+    def test_divide(self):
+        assert divide(1, 2) == 3 
+        assert divide(1, "asdf") == "Invalid Input"
+
